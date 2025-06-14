8f5df6070162119124cbf929fab783afdee64b43
Ioanna Kyzeridou <joannakyz45@gmail.com>
2025-06-14 13:24:14 +0300

    Simple character checker

--- char_checker.cpp
+++ char_checker.cpp
@@ -6,7 +6,7 @@ int main(){
 	cout<<"Enter character: ";
 	cin>>a;
 	
-	if ((a > 'a' && a < 'z') || (a > 'A' && a < 'Z')){
+	if ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z')){
 		cout<<"You have entered character: "<<a;
 	}else if (a >= '0' or a <= '9'){
 		cout<<"You entered a number.";
